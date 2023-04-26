1d3ef9084cf2f70c965f0dca06065c0097861d54
dechanezt <dechanezt@gmail.com>
2023-04-26 11:12:36 +0200

    With texture and suposed mesh

--- unity/Assets/OnCollisionEnter.cs
+++ unity/Assets/OnCollisionEnter.cs
@@ -27,6 +27,31 @@ public class OnCollisionEnter : MonoBehaviour
             // Assign the Portal material to the new cube object
             Renderer renderer = newObject.GetComponent<Renderer>();
             renderer.material = portalMaterial;
+
+            // Start a coroutine to gradually scale down the new object before deleting it
+            StartCoroutine(ScaleDownAndDestroy(newObject));
         }
     }
+
+    IEnumerator ScaleDownAndDestroy(GameObject objectToDestroy)
+    {
+        float scaleSpeed = 2f; // adjust this value to change the speed of scaling down
+        float scaleDuration = 1f; // adjust this value to change the duration of scaling down
+
+        // Gradually scale down the object over the set duration
+        for (float t = 0; t < scaleDuration; t += Time.deltaTime)
+        {
+            float scaleFactor = Mathf.Lerp(0.1f, 0f, t / scaleDuration);
+            objectToDestroy.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+            yield return null;
+        }
+
+        // Set the scale to exactly 0 to avoid floating-point precision issues
+        objectToDestroy.transform.localScale = Vector3.zero;
+
+        // Destroy the object
+        Destroy(objectToDestroy);
+    }
+
+
 }
